b04c96856763ae3a05d1f0090e59cce8284fb356
Spark Knight <jianji2019@foxmail.com>
2024-06-30 22:24:14 +0800

    FEAT: Add uwb data type into msgs.h.

--- center_module/Center_Module/src/msgs.cpp
+++ center_module/Center_Module/src/msgs.cpp
@@ -1,1 +1,46 @@
 #include "msgs.h"
+
+msgs::uwb_data::uwb_data(uint16_t self_id, uint16_t target_id, uint8_t CRC8):
+                        header(0xff), self_id(self_id),target_id(target_id), CRC8(CRC8) {
+    this->data = std::vector<uint8_t>(8);
+    this->CRC16 = 0x0F;
+}
+
+msgs::serials msgs::uwb_data::serialize() {
+    uint16_t data_len = this->data.size();
+    auto data = std::shared_ptr<uint8_t[]>(new uint8_t[10 + data_len]);
+    uint32_t current_size = 0;
+
+    auto pos = data.get();
+    current_size = (sizeof(uint16_t) + sizeof(uint8_t)) << 1;
+    memcpy(pos, &(this->header), current_size);
+    pos += current_size;
+
+    current_size = sizeof(uint16_t);
+    memcpy(pos, &data_len, current_size);
+    pos += current_size;
+
+    current_size = data_len * sizeof(uint8_t);
+    memcpy(pos, this->data.data(), current_size);
+    pos += current_size;
+
+    current_size = sizeof(uint16_t);
+    memcpy(pos, &(this->CRC16), current_size);
+    pos += current_size;
+    data_len += 10;
+
+    return msgs::serials{
+        .len = data_len,
+        .data_ptr = data
+    };
+}
+
+void msgs::uwb_data::set_data(std::vector<uint8_t> data) {
+    this->data = data;
+}
+
+void msgs::uwb_data::set_data(const float x, const float y) {
+    data = std::vector<uint8_t>(8);
+    std::memcpy(&data[0], &x, sizeof(float));
+    std::memcpy(&data[4], &y, sizeof(float));
+}
