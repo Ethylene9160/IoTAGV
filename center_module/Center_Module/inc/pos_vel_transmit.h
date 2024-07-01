1747ceac3ae751313b5cca61f028aae71f98e83e
Spark Knight <jianji2019@foxmail.com>
2024-07-01 10:47:52 +0800

    FEAT: position2velocity.

--- center_module/Center_Module/inc/pos_vel_transmit.h
+++ center_module/Center_Module/inc/pos_vel_transmit.h
@@ -1,49 +1,17 @@
-#ifndef VELOCITY_BIAS_H
-#define VELOCITY_BIAS_H
-#include <math.h>
-#include <vector>
+#ifndef CENTER_MODULE_POS_VEL_TRANSMIT_H_
+#define CENTER_MODULE_POS_VEL_TRANSMIT_H_
 
-namespace pos_vel_trainsmit {
-    struct polar_point {
-        float r;
-        float t;
-    };
-    struct cart_point {
-        float x;
-        float y;
-    };
+#include "uwb_tool.h"
 
-    class cart_velocity {
-    public:
+class cart_velocity {
+public:
+    void update_vel(cart_point& target);
 
-        void update_vel(std::vector<cart_point>& targets) {
-            for(cart_point& p:targets) {
-                update_vel(p);
-            }
-        }
+public:
+    float vx;
+    float vy;
+    float w;
+    cart_point p;
+};
 
-        void update_vel(cart_point& target) {
-            static float _w_k = 1.0f;
-            static float _w_bias = 0.05f;
-
-            float _delta_x = this->p.x - target.x;
-            float _delta_y = this->p.y - target.y;
-
-            float _k = 1.0f/((_delta_x * _delta_x) + (_delta_y * _delta_y));
-            float _theta = atan2(_delta_y, _delta_x);
-
-            this->vx += _k * _delta_x;
-            this->vy += _k * _delta_y;
-            this->w += _w_k * _theta + _w_bias;
-        }
-
-    public:
-        float vx;
-        float vy;
-        float w;
-        cart_point p;
-    };
-
-}
-
-#endif //VELOCITY_BIAS_H
+#endif
--- center_module/Center_Module/src/pos_vel_transmit.cpp
+++ center_module/Center_Module/src/pos_vel_transmit.cpp
@@ -1,1 +1,18 @@
 #include "pos_vel_transmit.h"
+#include <cstdint>
+#include <math.h>
+
+void cart_velocity::update_vel(cart_point& target) {
+    static float _w_k = 1.0f;
+    static float _w_bias = 0.05f;
+
+    float _delta_x = this->p.x - target.x;
+    float _delta_y = this->p.y - target.y;
+
+    float _k = 1.0f/((_delta_x * _delta_x) + (_delta_y * _delta_y));
+    float _theta = atan2(_delta_y, _delta_x);
+
+    this->vx += _k * _delta_x;
+    this->vy += _k * _delta_y;
+    this->w += _w_k * _theta + _w_bias;
+}
