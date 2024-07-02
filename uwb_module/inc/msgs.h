8ebfc048eb5c315077853deadac027e200bf3a86
gralerfics <glverfer@outlook.com>
2024-07-02 14:23:10 +0800

    bkup(ranging): add ranging task queuing.

--- uwb_module/inc/msgs.h
+++ uwb_module/inc/msgs.h
@@ -9,12 +9,6 @@ extern "C" {
 
 #define RANGING_EXCHANGE_MSG_SYNC_BYTE 0x5A
 
-#define RANGING_EXCHANGE_MSG_TASK_ID_MAX 128
-
-#define RANGING_EXCHANGE_MSG_TYPE_POLL 0x01
-#define RANGING_EXCHANGE_MSG_TYPE_RESPONSE 0x02
-#define RANGING_EXCHANGE_MSG_TYPE_FINAL 0x03
-
 uint16_t gen_ranging_exchange_msg(uint8_t *buf, uint16_t pan_id, uint16_t dest_id, uint16_t src_id, uint8_t task_id, uint8_t msg_type, uint8_t *payload, uint16_t payload_len);
 
 #ifdef __cplusplus
