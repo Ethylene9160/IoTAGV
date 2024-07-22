036ced65a6b97f910954cee26f4b3dc725385ed3
gralerfics <glverfer@outlook.com>
2024-07-22 11:04:17 +0800

    feat(visualization): three-point visualization.

--- uwb_module/utils/localizationSerialVisualization.py
+++ uwb_module/utils/localizationSerialVisualization.py
@@ -2,46 +2,61 @@ import serial
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
-
 import re
+
+FACTOR = 10000
+XMIN = -1
+XMAX = 3
+YMIN = 0
+YMAX = 5
+ARROW_WIDTH = 0.0005
+RADIUS = 0.3
 
 def extract_numbers(string):
     numbers = re.findall(r'\d+', string)
     return list(map(int, numbers[:4]))
 
 class SerialPlotter:
-    def __init__(self, port, baudrate=9600):
+    def __init__(self, port, baudrate=115200):
         self.ser = serial.Serial(port, baudrate, timeout=1)
-        self.x_data = []
-        self.y_data = []
-        self.filtered_x = 0.0
-        self.filtered_y = 0.0
-        self.x_data2 = []
-        self.y_data2 = []
-        self.filtered_x2 = 0.0
-        self.filtered_y2 = 0.0
+        self.dot0_x = 0.0
+        self.dot0_y = 0.0
+        self.dot1_x = 0.0
+        self.dot1_y = 0.0
+        self.dot2_x = 0.0
+        self.dot2_y = 0.0
 
         self.vx = 0.0
         self.vy = 0.0
 
         self.fig, self.ax = plt.subplots()
-        self.red_dot, = self.ax.plot([], [], 'ro')
-        self.blue_dot, = self.ax.plot([], [], 'bo')
-        self.green_dot, = self.ax.plot([], [], 'go')
-        self.blue_dot2, = self.ax.plot([], [], 'bo')
-        self.arrow = patches.FancyArrow(0, 0, 0, 0, width=50, color='green')
+        self.dot0, = self.ax.plot([], [], 'ro')
+        self.dot1, = self.ax.plot([], [], 'go')
+        self.dot2, = self.ax.plot([], [], 'bo')
+        self.arrow = patches.FancyArrow(0, 0, 0, 0, width=ARROW_WIDTH, color='green')
         self.ax.add_patch(self.arrow)
+
+        self.circle0 = patches.Circle((0, 0), RADIUS, color='red', fill=False, linestyle='dashed')
+        self.circle1 = patches.Circle((0, 0), RADIUS, color='green', fill=False, linestyle='dashed')
+        self.circle2 = patches.Circle((0, 0), RADIUS, color='blue', fill=False, linestyle='dashed')
+        self.ax.add_patch(self.circle0)
+        self.ax.add_patch(self.circle1)
+        self.ax.add_patch(self.circle2)
 
         self.ani = animation.FuncAnimation(self.fig, self.update_plot, interval=1, init_func=self.init_plot, blit=True)
 
     def init_plot(self):
-        self.ax.set_xlim(-10000, 30000)
-        self.ax.set_ylim(0, 50000)
-        self.red_dot.set_data([], [])
-        self.blue_dot.set_data([], [])
-        self.green_dot.set_data([], [])
-        self.blue_dot2.set_data([], [])
-        return self.red_dot, self.blue_dot, self.green_dot, self.blue_dot2, self.arrow
+        self.ax.set_xlim(XMIN, XMAX)
+        self.ax.set_ylim(YMIN, YMAX)
+        self.ax.set_aspect('equal')
+        self.dot0.set_data([], [])
+        self.dot1.set_data([], [])
+        self.dot2.set_data([], [])
+        self.circle0.center = (0, 0)
+        self.circle1.center = (0, 0)
+        self.circle2.center = (0, 0)
+        
+        return self.dot0, self.dot1, self.dot2, self.arrow, self.circle0, self.circle1, self.circle2
 
     def read_serial(self):
         if self.ser.in_waiting > 0:
@@ -51,45 +66,45 @@ class SerialPlotter:
                 
                 if num_list[0] == 0:
                     id, x, y = num_list[1:4]
+                    x /= FACTOR
+                    y /= FACTOR
                     
                     if id == 128:
-                        self.x_data = [x]
-                        self.y_data = [y]
+                        self.dot0_x = x
+                        self.dot0_y = y
                     elif id == 129:
-                        self.x_data2 = [x]
-                        self.y_data2 = [y]
-                    self.apply_filter(id, x, y)
+                        self.dot1_x = x
+                        self.dot1_y = y
+                    elif id == 130:
+                        self.dot2_x = x
+                        self.dot2_y = y
                     print(id, ' ', x, ' ', y)
                 elif num_list[0] == 1:
                     vx, vy = num_list[1:3]
-                    self.vx = (vx - 50000) / 4
-                    self.vy = (vy - 50000) / 4
-                
+                    vx /= FACTOR
+                    vy /= FACTOR
+                    
+                    self.vx = (vx - 5) / 4
+                    self.vy = (vy - 5) / 4
+            
             except Exception as e:
                 print(f"Error reading line: {e}")
 
-    def apply_filter(self, id, x, y):
-        alpha = 0.2
-        if id == 128:
-            self.filtered_x = alpha * x + (1 - alpha) * self.filtered_x
-            self.filtered_y = alpha * y + (1 - alpha) * self.filtered_y
-        elif id == 129:
-            self.filtered_x2 = alpha * x + (1 - alpha) * self.filtered_x2
-            self.filtered_y2 = alpha * y + (1 - alpha) * self.filtered_y2
-
     def update_plot(self, frame):
         self.read_serial()
-        self.red_dot.set_data(self.x_data, self.y_data)
-        self.blue_dot.set_data([self.filtered_x], [self.filtered_y])
-        self.green_dot.set_data(self.x_data2, self.y_data2)
-        self.blue_dot2.set_data([self.filtered_x2], [self.filtered_y2])
+        self.dot0.set_data(self.dot0_x, self.dot0_y)
+        self.dot1.set_data(self.dot1_x, self.dot1_y)
+        self.dot2.set_data(self.dot2_x, self.dot2_y)
 
-        # 更新箭头位置和方向
         self.arrow.remove()
-        self.arrow = patches.FancyArrow(self.filtered_x, self.filtered_y, self.vx, self.vy, width=50, color='green')
+        self.arrow = patches.FancyArrow(self.dot0_x, self.dot0_y, self.vx, self.vy, width=ARROW_WIDTH, color='green')
         self.ax.add_patch(self.arrow)
+
+        self.circle0.center = (self.dot0_x, self.dot0_y)
+        self.circle1.center = (self.dot1_x, self.dot1_y)
+        self.circle2.center = (self.dot2_x, self.dot2_y)
         
-        return self.red_dot, self.blue_dot, self.green_dot, self.blue_dot2, self.arrow
+        return self.dot0, self.dot1, self.dot2, self.arrow, self.circle0, self.circle1, self.circle2
 
     def start(self):
         plt.show()
